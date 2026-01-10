ae6109a8910090731fe133054e27653a86f7ea97
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2026-01-10 15:27:26 -0800

    Add main script for package

---
+++ src/segmentation/__main__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2026 Murali Dhanakoti
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+from segmentation import segment
+
+if __name__ == "__main__":
+    segment.main()
